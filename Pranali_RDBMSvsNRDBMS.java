6b0ca6427f0fbf50c6bb116b798187b61f3ab74c
pranali <pranalikubade2000@gmail.com>
2023-09-22 15:02:29 +0530

    [Pranali: Add RDBMSvsNRDBMS Added]

--- Pranali_RDBMSvsNRDBMS.java
+++ Pranali_RDBMSvsNRDBMS.java
@@ -1,6 +1,6 @@
 class Pranali_RDBMSvsNRDBMS {
     public static void main(String[] args) {
-
+s
         System.out.println("1.Data Structure:");
 
 System.out.println("RDBMS: RDBMS stores data in structured tables with rows and columns. It uses a predefined schema to define the data structure.");
@@ -27,7 +27,23 @@ System.out.println("5.Use Cases:");
 System.out.println("RDBMS: RDBMS is well-suited for applications with structured data, complex relationships, and strict data integrity requirements. Common in traditional business applications, finance, and e-commerce.");
 System.out.println("NRDBMS: NRDBMS is a better choice for applications with rapidly changing data models, unstructured or semi-structured data, and the need for horizontal scalability. Common in big data, real-time analytics, and content management systems.");
 
+System.out.println(" MySQL (an RDBMS) Vs MongoDB (a NoSQL database):");
 
+System.out.println(" Data Model:"
+                "MySQL: Uses a structured, tabular data model with tables, rows, and columns.
+                "MongoDB: Uses a document-oriented data model where data is stored in JSON-like documents.");
+System.out.println(" Schema:
+                "MySQL: Requires a predefined schema with fixed data types and relationships between tables.
+                "MongoDB: Has a flexible schema, allowing for varying data structures within the same collection (similar to a table).");
+System.out.println(" Query Language:
+                "MySQL: Utilizes SQL, which is powerful for complex querying and joins.
+                "MongoDB: Uses a query language specific to JSON-like documents, which is suitable for simple queries and document manipulation.");
+System.out.println(" Scaling:
+                "MySQL: Scales vertically, typically by adding more CPU, memory, or storage to a single server.
+                "MongoDB: Scales horizontally by adding more servers or nodes to a cluster, making it well-suited for distributed, high-volume workloads.");
+System.out.println(" Use Cases:
+                "MySQL: Commonly used for applications with structured data and complex relationships, such as e-commerce systems, content management systems, and financial applications.
+                "MongoDB: Well-suited for applications with rapidly evolving data, unstructured or semi-structured data, real-time analytics, and large-scale content management systems.");
 
     }
 }
